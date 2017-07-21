07c89389d47182dd8a62b0695a88a03695fd4356
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-21 16:00:15 +0200

    changed from rectangles to lines to draw

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var elem = document.getElementById("canvas"),
   elemLeft = elem.offsetLeft,
   elemTop = elem.offsetTop,
   ctx = elem.getContext('2d'),
+  prevX = 0,
+  prevY = 0,
   currX = 0,
   currY = 0,
   clicked = false;
@@ -10,19 +12,34 @@ $("#canvas").mousemove(function(event) {
   if (clicked) {
     currX = event.pageX - elemLeft;
     currY = event.pageY - elemTop;
-    ctx.beginPath();
-    ctx.fillStyle = "black";
-    ctx.fillRect(currX, currY, 5, 5);
+    ctx.lineTo(currX, currY);
+    ctx.lineWidth = $("select").val()[3];
+    ctx.lineCap = "round";
     ctx.stroke();
-    ctx.closePath();
   }
 });
 
 
-$("#canvas").mousedown(function() {
+$("#canvas").mousedown(function(event) {
   clicked = true;
+  prevX = event.pageX - elemLeft;
+  prevY = event.pageY - elemTop;
+  ctx.beginPath();
+  ctx.strokeStyle = "blue";
+  ctx.moveTo(prevX, prevY);
+});
+
+$("#canvas").mouseout(function(){
+  clicked = false;
+  ctx.closePath();
 });
 
 $("#canvas").mouseup(function() {
   clicked = false;
+  ctx.closePath();
 });
+
+
+// .arc() .fill()
+// linecap > square , round
+//ctx.fillRect(currX, currY, $("select").val()[3], $("select").val()[3]);
