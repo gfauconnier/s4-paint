7873160f10c4db24074043e2cb9d4a6dd6e11253
Greg Fauconnier <g.fauconnier@hotmail.fr>
2017-07-24 11:25:20 +0200

    index.html : added divs for placement + buttons to chose the drawing style - main.css : did positionning of new elements + worked on responsive - main.js : added a function to change style+drawing type of buttons + made rect and circle to be drawn

--- js/main.js
+++ js/main.js
@@ -7,45 +7,74 @@ var elem = document.getElementById("canvas"),
   currX = 0,
   currY = 0,
   clicked = false,
-  brushSize = 1;
-  //brushColor = "#000000";
+  brushSize = 1,
+  drawStyle = 0;
 
-
-$("#canvas").mousemove(function(event) {
-  if (clicked) {
-    currX = event.pageX - elemLeft;
-    currY = event.pageY - elemTop;
-    ctx.lineTo(currX, currY);
-    ctx.lineWidth = brushSize;
-    ctx.lineCap = "round";
-    ctx.stroke();
-  }
-});
+elem.height = $("#canvas").height();
+elem.width = $("#canvas").width();
 
 
 $("#canvas").mousedown(function(event) {
   clicked = true;
   prevX = event.pageX - elemLeft;
   prevY = event.pageY - elemTop;
-  ctx.beginPath();
   ctx.strokeStyle = $("#colorpicker").val();
-  ctx.moveTo(prevX, prevY);
+  ctx.lineWidth = brushSize;
+  if (drawStyle == 0) {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+  } else if (drawStyle == 1) {
+    ctx.strokeRect(prevX, prevY, $(".drawtype>input").eq(0).val(), $(".drawtype>input").eq(1).val());
+  } else {
+    ctx.beginPath();
+    ctx.arc(prevX, prevY, $(".drawtype>input").eq(2).val(), 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 });
 
 
-$("#canvas").mouseout(function(){
+$("#canvas").mousemove(function(event) {
+  if (clicked) {
+    currX = event.pageX - elemLeft;
+    currY = event.pageY - elemTop;
+    if (drawStyle == 0) {
+      ctx.lineTo(currX, currY);
+      ctx.lineCap = "round";
+      ctx.stroke();
+    }
+  }
+});
+
+
+$("#canvas").mouseout(function() {
   clicked = false;
-  ctx.closePath();
+  if (drawStyle == 0) {
+    ctx.closePath();
+  }
 });
 
 
 $("#canvas").mouseup(function() {
   clicked = false;
-  ctx.closePath();
+  if (drawStyle == 0) {
+    ctx.closePath();
+  }
+});
+
+$(".drawtype>button").click(function() {
+  drawStyle = $(".drawtype>button").index(this);
+  $(".drawtype>button").removeClass("btn-success");
+  $(".drawtype>button").addClass("btn-secondary");
+  $(".drawtype>button").eq(drawStyle).addClass("btn-success");
 });
 
 
-$("#brushsize").click(function(){
+$("#brushsize").click(function() {
   brushSize = $("#brushsize").val();
   $(".labelsize").text("Brush size : " + brushSize + "px");
+});
+
+
+$("#clearbtn").click(function() {
+  ctx.clearRect(0, 0, elem.width, elem.height);
 });
